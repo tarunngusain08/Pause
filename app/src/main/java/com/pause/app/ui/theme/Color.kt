e748fa0b5c2c306710a0de5898303e88985b5514
tarunngusain08 <prudent.tarun0808@gmail.com>
2026-03-06 08:17:36 +0530

    adding the docs and app changes: Color.kt

---
+++ app/src/main/java/com/pause/app/ui/theme/Color.kt
@@ -0,0 +1,8 @@
+package com.pause.app.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val SoftBlue = Color(0xFF4A90A4)
+val SoftBlueDark = Color(0xFF2E5F6B)
+val CalmBackground = Color(0xFFF0F4F8)
+val CalmSurface = Color(0xFFE8F4F7)
