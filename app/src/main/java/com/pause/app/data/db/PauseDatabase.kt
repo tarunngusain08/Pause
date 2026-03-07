57b2c6c368e5519b0cd7a38db4fddd115898126e
tarunngusain08 <prudent.tarun0808@gmail.com>
2026-03-07 08:18:32 +0530

    adding the docs and app changes: PauseDatabase.kt

--- app/src/main/java/com/pause/app/data/db/PauseDatabase.kt
+++ app/src/main/java/com/pause/app/data/db/PauseDatabase.kt
@@ -6,15 +6,25 @@ import androidx.room.TypeConverters
 import com.pause.app.data.db.dao.AccountabilityDao
 import com.pause.app.data.db.dao.LaunchEventDao
 import com.pause.app.data.db.dao.MonitoredAppDao
+import com.pause.app.data.db.dao.ParentalBlockedAppDao
+import com.pause.app.data.db.dao.ParentalConfigDao
+import com.pause.app.data.db.dao.PINAuditLogDao
 import com.pause.app.data.db.dao.ReflectionResponseDao
+import com.pause.app.data.db.dao.ScheduleBandDao
 import com.pause.app.data.db.dao.SessionDao
+import com.pause.app.data.db.dao.StrictBreakLogDao
 import com.pause.app.data.db.dao.StreakDao
 import com.pause.app.data.db.dao.UnlockEventDao
 import com.pause.app.data.db.entity.Accountability
 import com.pause.app.data.db.entity.LaunchEvent
 import com.pause.app.data.db.entity.MonitoredApp
+import com.pause.app.data.db.entity.ParentalBlockedApp
+import com.pause.app.data.db.entity.ParentalConfig
+import com.pause.app.data.db.entity.PINAuditLog
 import com.pause.app.data.db.entity.ReflectionResponse
+import com.pause.app.data.db.entity.ScheduleBandEntity
 import com.pause.app.data.db.entity.Session
+import com.pause.app.data.db.entity.StrictBreakLog
 import com.pause.app.data.db.entity.Streak
 import com.pause.app.data.db.entity.UnlockEvent
 
@@ -26,9 +36,14 @@ import com.pause.app.data.db.entity.UnlockEvent
         Streak::class,
         ReflectionResponse::class,
         UnlockEvent::class,
-        Accountability::class
+        Accountability::class,
+        StrictBreakLog::class,
+        ParentalConfig::class,
+        ScheduleBandEntity::class,
+        ParentalBlockedApp::class,
+        PINAuditLog::class
     ],
-    version = 1,
+    version = 2,
     exportSchema = false
 )
 @TypeConverters(Converters::class)
@@ -41,4 +56,9 @@ abstract class PauseDatabase : RoomDatabase() {
     abstract fun reflectionResponseDao(): ReflectionResponseDao
     abstract fun unlockEventDao(): UnlockEventDao
     abstract fun accountabilityDao(): AccountabilityDao
+    abstract fun strictBreakLogDao(): StrictBreakLogDao
+    abstract fun parentalConfigDao(): ParentalConfigDao
+    abstract fun scheduleBandDao(): ScheduleBandDao
+    abstract fun parentalBlockedAppDao(): ParentalBlockedAppDao
+    abstract fun pinAuditLogDao(): PINAuditLogDao
 }
