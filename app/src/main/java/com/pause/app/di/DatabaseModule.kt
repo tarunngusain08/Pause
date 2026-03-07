04be42638a0386547b1246f3b6755c81e7ba7464
tarunngusain08 <prudent.tarun0808@gmail.com>
2026-03-07 08:18:33 +0530

    adding the docs and app changes: DatabaseModule.kt

--- app/src/main/java/com/pause/app/di/DatabaseModule.kt
+++ app/src/main/java/com/pause/app/di/DatabaseModule.kt
@@ -6,8 +6,13 @@ import com.pause.app.data.db.PauseDatabase
 import com.pause.app.data.db.dao.AccountabilityDao
 import com.pause.app.data.db.dao.LaunchEventDao
 import com.pause.app.data.db.dao.MonitoredAppDao
+import com.pause.app.data.db.dao.ParentalBlockedAppDao
+import com.pause.app.data.db.dao.ParentalConfigDao
+import com.pause.app.data.db.dao.PINAuditLogDao
 import com.pause.app.data.db.dao.ReflectionResponseDao
+import com.pause.app.data.db.dao.ScheduleBandDao
 import com.pause.app.data.db.dao.SessionDao
+import com.pause.app.data.db.dao.StrictBreakLogDao
 import com.pause.app.data.db.dao.StreakDao
 import com.pause.app.data.db.dao.UnlockEventDao
 import dagger.Module
@@ -23,10 +28,16 @@ object DatabaseModule {
 
     @Provides
     @Singleton
-    fun provideDatabase(@ApplicationContext context: Context): PauseDatabase =
-        Room.databaseBuilder(context, PauseDatabase::class.java, "pause_db")
+    fun provideDatabase(@ApplicationContext context: Context): PauseDatabase {
+        val dbContext = if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+            context.createDeviceProtectedStorageContext()
+        } else {
+            context
+        }
+        return Room.databaseBuilder(dbContext, PauseDatabase::class.java, "pause_db")
             .fallbackToDestructiveMigration()
             .build()
+    }
 
     @Provides
     @Singleton
@@ -56,4 +67,25 @@ object DatabaseModule {
     @Provides
     @Singleton
     fun provideAccountabilityDao(db: PauseDatabase): AccountabilityDao = db.accountabilityDao()
+
+    @Provides
+    @Singleton
+    fun provideStrictBreakLogDao(db: PauseDatabase): StrictBreakLogDao = db.strictBreakLogDao()
+
+    @Provides
+    @Singleton
+    fun provideParentalConfigDao(db: PauseDatabase): ParentalConfigDao = db.parentalConfigDao()
+
+    @Provides
+    @Singleton
+    fun provideScheduleBandDao(db: PauseDatabase): ScheduleBandDao = db.scheduleBandDao()
+
+    @Provides
+    @Singleton
+    fun provideParentalBlockedAppDao(db: PauseDatabase): ParentalBlockedAppDao =
+        db.parentalBlockedAppDao()
+
+    @Provides
+    @Singleton
+    fun providePINAuditLogDao(db: PauseDatabase): PINAuditLogDao = db.pinAuditLogDao()
 }
