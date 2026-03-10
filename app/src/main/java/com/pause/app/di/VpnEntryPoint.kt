cde2997cda1b6630a3bcbf48d68bfd8c3c337576
tarunngusain08 <prudent.tarun0808@gmail.com>
2026-03-10 20:17:12 +0530

    adding the docs and app changes: VpnEntryPoint.kt

---
+++ app/src/main/java/com/pause/app/di/VpnEntryPoint.kt
@@ -0,0 +1,20 @@
+package com.pause.app.di
+
+import com.pause.app.data.repository.BlacklistRepository
+import com.pause.app.service.webfilter.BlocklistMatcher
+import com.pause.app.service.webfilter.WhitelistMatcher
+import com.pause.app.data.repository.WhitelistRepository
+import com.pause.app.data.repository.WebFilterConfigRepository
+import dagger.hilt.EntryPoint
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@EntryPoint
+@InstallIn(SingletonComponent::class)
+interface VpnEntryPoint {
+    fun getBlacklistRepository(): BlacklistRepository
+    fun getWhitelistRepository(): WhitelistRepository
+    fun getWebFilterConfigRepository(): WebFilterConfigRepository
+    fun getBlocklistMatcher(): BlocklistMatcher
+    fun getWhitelistMatcher(): WhitelistMatcher
+}
