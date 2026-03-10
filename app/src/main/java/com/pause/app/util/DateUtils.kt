50f6e8d94390abba3f45289ccd98eb91b5d1c197
tarunngusain08 <prudent.tarun0808@gmail.com>
2026-03-10 20:17:13 +0530

    adding the docs and app changes: DateUtils.kt

--- app/src/main/java/com/pause/app/util/DateUtils.kt
+++ app/src/main/java/com/pause/app/util/DateUtils.kt
@@ -13,6 +13,16 @@ object DateUtils {
         return cal.timeInMillis
     }
 
+    fun getYesterdayMidnight(): Long {
+        val cal = Calendar.getInstance()
+        cal.add(Calendar.DAY_OF_YEAR, -1)
+        cal.set(Calendar.HOUR_OF_DAY, 0)
+        cal.set(Calendar.MINUTE, 0)
+        cal.set(Calendar.SECOND, 0)
+        cal.set(Calendar.MILLISECOND, 0)
+        return cal.timeInMillis
+    }
+
     fun getWeekAgoMidnight(): Long {
         val cal = Calendar.getInstance()
         cal.add(Calendar.DAY_OF_YEAR, -7)
