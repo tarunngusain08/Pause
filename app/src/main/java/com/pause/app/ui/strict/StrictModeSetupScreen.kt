045bc442c2fe89f2ba92e0a3ec63f8fdc54eca1e
tarunngusain08 <prudent.tarun0808@gmail.com>
2026-03-11 13:46:38 +0530

    simplifying the app and resolving confusion: StrictModeSetupScreen.kt

--- app/src/main/java/com/pause/app/ui/strict/StrictModeSetupScreen.kt
+++ app/src/main/java/com/pause/app/ui/strict/StrictModeSetupScreen.kt
@@ -5,10 +5,12 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.rememberScrollState
@@ -55,6 +57,7 @@ fun StrictModeSetupScreen(
     Column(
         modifier = Modifier
             .fillMaxSize()
+            .windowInsetsPadding(WindowInsets.statusBars)
             .padding(24.dp)
             .verticalScroll(rememberScrollState())
     ) {
