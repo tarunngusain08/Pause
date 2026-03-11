ae3ebf5cc9a12a7046f758c6c5bd395534f458a6
tarunngusain08 <prudent.tarun0808@gmail.com>
2026-03-11 13:46:39 +0530

    simplifying the app and resolving confusion: UrlVisitLogScreen.kt

--- app/src/main/java/com/pause/app/ui/webfilter/UrlVisitLogScreen.kt
+++ app/src/main/java/com/pause/app/ui/webfilter/UrlVisitLogScreen.kt
@@ -5,10 +5,12 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
@@ -39,6 +41,7 @@ fun UrlVisitLogScreen(
     Column(
         modifier = Modifier
             .fillMaxSize()
+            .windowInsetsPadding(WindowInsets.statusBars)
             .padding(24.dp)
     ) {
         Text(
