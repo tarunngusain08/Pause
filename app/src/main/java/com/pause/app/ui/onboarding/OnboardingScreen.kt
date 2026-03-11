4199e021a7d12c6d3a71e596981c75511ebd822d
tarunngusain08 <prudent.tarun0808@gmail.com>
2026-03-11 13:46:38 +0530

    simplifying the app and resolving confusion: OnboardingScreen.kt

--- app/src/main/java/com/pause/app/ui/onboarding/OnboardingScreen.kt
+++ app/src/main/java/com/pause/app/ui/onboarding/OnboardingScreen.kt
@@ -18,10 +18,12 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
@@ -91,6 +93,7 @@ fun OnboardingScreen(
     Column(
         modifier = Modifier
             .fillMaxSize()
+            .windowInsetsPadding(WindowInsets.statusBars)
             .padding(24.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
