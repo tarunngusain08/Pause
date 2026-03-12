5d12485b38ddd7a600b0afab71bed2265c6b336b
tarunngusain08 <prudent.tarun0808@gmail.com>
2026-03-12 08:09:00 +0530

    simplifying the app and resolving confusion: PauseAccessibilityEntryPoint.kt

--- app/src/main/java/com/pause/app/di/PauseAccessibilityEntryPoint.kt
+++ app/src/main/java/com/pause/app/di/PauseAccessibilityEntryPoint.kt
@@ -1,14 +1,10 @@
 package com.pause.app.di
 
-import com.pause.app.data.preferences.PreferencesManager
-import com.pause.app.data.repository.AppRepository
 import com.pause.app.data.repository.InsightsRepository
-import com.pause.app.data.repository.LaunchRepository
-import com.pause.app.data.repository.SessionRepository
-import com.pause.app.service.AllowanceTracker
 import com.pause.app.data.repository.ParentalBlockedAppRepository
 import com.pause.app.service.overlay.OverlayManager
 import com.pause.app.service.parental.ParentalControlManager
+import com.pause.app.service.contentshield.ContentShieldManager
 import com.pause.app.service.strict.StrictSessionManager
 import com.pause.app.data.repository.WebFilterConfigRepository
 import com.pause.app.service.webfilter.url.AutoBlacklistEngine
@@ -23,14 +19,10 @@ import dagger.hilt.components.SingletonComponent
 @InstallIn(SingletonComponent::class)
 interface PauseAccessibilityEntryPoint {
     fun getOverlayManager(): OverlayManager
-    fun getAppRepository(): AppRepository
-    fun getLaunchRepository(): LaunchRepository
-    fun getAllowanceTracker(): AllowanceTracker
-    fun getSessionRepository(): SessionRepository
     fun getParentalBlockedAppRepository(): ParentalBlockedAppRepository
-    fun getPreferencesManager(): PreferencesManager
     fun getStrictSessionManager(): StrictSessionManager
     fun getParentalControlManager(): ParentalControlManager
+    fun getContentShieldManager(): ContentShieldManager
     fun getInsightsRepository(): InsightsRepository
     fun getBrowserURLReader(): BrowserURLReader
     fun getURLClassifier(): URLClassifier
